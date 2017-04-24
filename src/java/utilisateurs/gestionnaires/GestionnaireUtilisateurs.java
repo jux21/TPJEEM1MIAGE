a41ab10d850853233f07af3d1a6c1330d3c6c246
Jux <julien.hubert.info@gmail.com>
2017-04-24 20:55:43 +0200

    UPDATE and DELETE change directory

---
+++ src/java/utilisateurs/gestionnaires/GestionnaireUtilisateurs.java
@@ -0,0 +1,58 @@
+package utilisateurs.gestionnaires;  
+  
+import java.util.Collection;  
+import javax.ejb.Stateless;  
+import javax.persistence.EntityManager;  
+import javax.persistence.PersistenceContext;  
+import javax.persistence.Query;  
+import utilisateurs.modeles.Utilisateur;  
+  
+@Stateless  
+public class GestionnaireUtilisateurs {  
+    // Ici injection de code : on n'initialise pas. L'entity manager sera créé  
+    // à partir du contenu de persistence.xml  
+    @PersistenceContext  
+    private EntityManager em;  
+  
+    public void creerUtilisateursDeTest() {  
+        creeUtilisateur("John", "Lennon", "jlennon");  
+        creeUtilisateur("Paul", "Mac Cartney", "pmc");  
+        creeUtilisateur("Ringo", "Starr", "rstarr");  
+        creeUtilisateur("Georges", "Harisson", "georgesH");  
+    }  
+  
+    public Utilisateur creeUtilisateur(String nom, String prenom, String login) {  
+        Utilisateur u = new Utilisateur(nom, prenom, login);  
+        em.persist(u);  
+        return u;  
+    }  
+  
+    public Collection<Utilisateur> getAllUsers() {  
+        // Exécution d'une requête équivalente à un select *  
+        Query q = em.createQuery("select u from Utilisateur u");  
+        return q.getResultList();  
+    } 
+    
+    public Collection<Utilisateur> getOneUserByLogin(String login) {
+        // Exécution d'une requête équivalente à un select where login  
+        Query q = em.createQuery("SELECT u FROM Utilisateur u WHERE u.login ='"+login+"'");  
+        return q.getResultList(); 
+    }
+    
+    public int updateUtilisateur(String nom, String prenom, String login) {
+        // Exécution d'une requête équivalente à un update where login    
+        Query q = em.createQuery("UPDATE Utilisateur u SET u.lastname = '"+prenom+"', u.firstname = '"+nom+"' WHERE u.login = '"+login+"'");
+        return q.executeUpdate();
+    }
+    
+    public int deleteUser(String login) {
+        // Exécution d'une requête équivalente à un delete from where login    
+        Query q = em.createQuery("DELETE FROM Utilisateur u WHERE u.login = '"+login+"'");
+        return q.executeUpdate();
+        /*Query query = em.createQuery(
+      "UPDATE Country SET population = population * 11 / 10 " +
+      "WHERE c.population < :p");*/
+    }
+    // Add business logic below. (Right-click in editor and choose  
+    // "Insert Code > Add Business Method")  
+}  
